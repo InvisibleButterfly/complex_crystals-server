7b68eb2949329a5cf4b3320c6c7e0eb2eaea7ee8
97nomad <97nomad@gmail.com>
2016-09-03 17:21:15 +0300

    Разбил на модули

--- src/main.rs
+++ src/main.rs
@@ -2,72 +2,25 @@ extern crate rustc_serialize;
 extern crate iron;
 
 mod game_engine;
+mod network_engine;
 
 use game_engine::GameEngine;
 use std::sync::{Arc, Mutex};
 use std::thread;
-use rustc_serialize::json;
-
-use std::collections::HashMap;
-
-use iron::prelude::*;
-use iron::Handler;
-use iron::status;
-
-struct Router {
-    routes: HashMap<String, Box<Handler>>,
-}
-
-impl Router {
-    fn new() -> Self {
-        Router { routes: HashMap::new() }
-    }
-
-    fn add_route<H>(&mut self, path: String, handler: H)
-        where H: Handler
-    {
-        self.routes.insert(path, Box::new(handler));
-    }
-}
-
-impl Handler for Router {
-    fn handle(&self, req: &mut Request) -> IronResult<Response> {
-        match self.routes.get(&req.url.path().join("/")) {
-            Some(handler) => handler.handle(req),
-            None => Ok(Response::with(status::NotFound)),
-        }
-    }
-}
 
 fn main() {
-    let engine = Arc::new(Mutex::new(GameEngine::new()));
+    let mutex_engine = Arc::new(Mutex::new(GameEngine::new()));
 
-    engine.lock().unwrap().add_object("Object1".to_string(), 10.0f32, 20.0f32);
-    engine.lock().unwrap().add_object("Object2".to_string(), 100.0f32, 10.0f32);
+    mutex_engine.lock().unwrap().add_object("Object1".to_string(), 10.0f32, 20.0f32);
+    mutex_engine.lock().unwrap().add_object("Object2".to_string(), 100.0f32, 10.0f32);
 
-    let mut router = Router::new();
+    let cloned_engine = mutex_engine.clone();
+    thread::spawn(move || network_engine::start(cloned_engine));
 
-    let cloned_engine = engine.clone();
-    router.add_route("objects".to_string(), move |_: &mut Request| {
-        let engine = cloned_engine.lock().unwrap();
-        let resp_json = json::encode(&engine.objects).unwrap();
-        Ok(Response::with((status::Ok, resp_json)))
-    });
-
-    let cloned_engine = engine.clone();
-    router.add_route("info".to_string(), move |_: &mut Request| {
-        let engine = cloned_engine.lock().unwrap();
-        let resp_json = json::encode(&engine.info).unwrap();
-        Ok(Response::with((status::Ok, resp_json)))
-    });
-
-    thread::spawn(|| Iron::new(router).http("localhost:3000").unwrap());
-
-    engine.lock().unwrap().add_object("Object3".to_string(), 100.0f32, 10.0f32);
+    mutex_engine.lock().unwrap().add_object("Object3".to_string(), 100.0f32, 10.0f32);
     println!("Test");
     loop {
-        engine.lock().unwrap().game_loop();
-        // let server_info = engine.info.clone();
-        // let resp_json = json::encode(&server_info).unwrap();
+        let mut engine = mutex_engine.lock().unwrap();
+        engine.game_loop();
     }
 }
--- src/network_engine/mod.rs
+++ src/network_engine/mod.rs
@@ -1,18 +1,68 @@
+use super::game_engine::GameEngine;
 
+use std::sync::{Arc, Mutex};
+use std::io::Read;
+use rustc_serialize::json;
 
-pub fn start(game: &'static game_engine::Game) {
-    let mut router = Router::new();
+use std::collections::HashMap;
 
+use iron::prelude::*;
+use iron::Handler;
+use iron::status;
 
-    router.add_route("object".to_string(), move |_: &mut Request| {
-        let resp_json = json::encode(&game.SomeObjects[0]).unwrap();
-        Ok(Response::with((status::Ok, resp_json)))
-    });
+struct Router {
+    routes: HashMap<String, Box<Handler>>,
 }
 
-// pub fn route_objects(_: &mut Request) -> IronResult<Response> {
-// let resp_json = json::encode(&game.get_objects()).unwrap();
-//
-// Ok(Response::with((status::Ok, resp_json)))
-// }
- 
+impl Router {
+    fn new() -> Self {
+        Router { routes: HashMap::new() }
+    }
+
+    fn add_route<H>(&mut self, path: String, handler: H)
+        where H: Handler
+    {
+        self.routes.insert(path, Box::new(handler));
+    }
+}
+
+impl Handler for Router {
+    fn handle(&self, req: &mut Request) -> IronResult<Response> {
+        match self.routes.get(&req.url.path().join("/")) {
+            Some(handler) => handler.handle(req),
+            None => Ok(Response::with(status::NotFound)),
+        }
+    }
+}
+
+pub fn start(mutex: Arc<Mutex<GameEngine>>) {
+    let mut router = Router::new();
+
+    let cloned_engine = mutex.clone();
+    router.add_route("objects".to_string(), move |_: &mut Request| {
+        let engine = cloned_engine.lock().unwrap();
+        let resp_json = json::encode(&engine.objects).unwrap();
+        Ok(Response::with((status::Ok, resp_json)))
+    });
+
+    let cloned_engine = mutex.clone();
+    router.add_route("info".to_string(), move |_: &mut Request| {
+        let engine = cloned_engine.lock().unwrap();
+        let resp_json = json::encode(&engine.info).unwrap();
+        Ok(Response::with((status::Ok, resp_json)))
+    });
+
+    let cloned_engine = mutex.clone();
+    router.add_route("set_info".to_string(), move |req: &mut Request| {
+        let mut engine = cloned_engine.lock().unwrap();
+        let mut buf = String::new();
+        req.body.read_to_string(&mut buf).unwrap();
+        match json::decode(&buf) {
+            Err(e) => println!("Json parsing error: {:?}", e),
+            Ok(info) => engine.info = info,
+        }
+        Ok(Response::with((status::Ok)))
+    });
+
+    Iron::new(router).http("localhost:3000").unwrap();
+}
